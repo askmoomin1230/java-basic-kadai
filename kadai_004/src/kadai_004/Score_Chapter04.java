f4912a78a69297addf84ae4e196bd863d87843fb
askmoomin1230 <144012465+askmoomin1230@users.noreply.github.com>
2023-10-19 11:00:50 +0900

    kadai_004
    kadai_004

---
+++ kadai_004/src/kadai_004/Score_Chapter04.java
@@ -0,0 +1,11 @@
+package kadai_004;
+
+public class Score_Chapter04 {
+
+	public static void main(String[] args) {
+		// テストの平均点
+		System.out.println((10 + 20 + 30 + 40 + 50 + 60 + 70 + 80 + 90 + 100) / 10);
+
+	}
+
+}
